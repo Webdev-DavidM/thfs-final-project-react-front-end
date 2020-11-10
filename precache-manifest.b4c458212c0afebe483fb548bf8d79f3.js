8bef95ca4bca0fda3defd0b20ac78e2673d08fd6
David Mulholland <davidmulholland@Davids-iMac.local>
2020-11-10 17:28:39 +0000

    Updates

---
+++ precache-manifest.b4c458212c0afebe483fb548bf8d79f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb19ec062929dbf50373b9edda75eaa",
+    "url": "/thfs-final-project-react-front-end/index.html"
+  },
+  {
+    "revision": "746b9efbcdd05f67f3c9",
+    "url": "/thfs-final-project-react-front-end/static/css/main.26217f7e.chunk.css"
+  },
+  {
+    "revision": "451ff0257580ba5d16ed",
+    "url": "/thfs-final-project-react-front-end/static/js/2.50468865.chunk.js"
+  },
+  {
+    "revision": "de81bfc3d566733bec20fcc6cadc32a6",
+    "url": "/thfs-final-project-react-front-end/static/js/2.50468865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "746b9efbcdd05f67f3c9",
+    "url": "/thfs-final-project-react-front-end/static/js/main.bd269724.chunk.js"
+  },
+  {
+    "revision": "56da8a0d63901a2010be",
+    "url": "/thfs-final-project-react-front-end/static/js/runtime-main.bc2a7b65.js"
+  }
+]);
