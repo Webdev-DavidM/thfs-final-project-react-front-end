f16024f5464daf9ee853732219f1207f28afb974
David Mulholland <davidmulholland@Davids-iMac.local>
2020-11-18 10:24:24 +0000

    corrected modal window typo error

--- src/components/ModalBox.js
+++ src/components/ModalBox.js
@@ -10,7 +10,7 @@ function ModalBox(props) {
           This website allows users to browse available courses. If you are
           signed in and have created a course, when you click on that course to
           view it, you can edit and delete it. You can view other people's
-          course but can't edit or delete them. You can log in as myself,
+          courses but can't edit or delete them. You can log in as myself,
           username davidm.webdeveloper@gmail.com, password admin123 to amend and
           delete my courses, or create a profile yourself and create your own
           courses.
